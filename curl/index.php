8e7c5365466a39d4ebf5f1702de1c2eeb168d0c4
Phạm Thanh Danh <47974698+thanhdanh27600@users.noreply.github.com>
2022-01-26 20:45:40 +0700

    #crud app

--- curl/index.php
+++ curl/index.php
@@ -1,22 +1,22 @@
 <?php
 
 $url = 'https://jsonplaceholder.typicode.com/users';
-// Sample example to get data.
-//$resource = curl_init($url);
-//curl_setopt($resource, CURLOPT_RETURNTRANSFER, true);
-//$usersJSON = curl_exec($resource);
-//echo $usersJSON;
-//curl_close($resource);
-//
-//// Get response status code
-//$responseCode = curl_getinfo($resource, CURLINFO_HTTP_CODE);
-//echo $responseCode;
+// // Sample example to get data.
+// $resource = curl_init($url);
+// curl_setopt($resource, CURLOPT_RETURNTRANSFER, true);
+// $usersJSON = curl_exec($resource);
+// // echo $usersJSON;
+// // curl_close($resource);
+
+// // // Get response status code
+// $responseCode = curl_getinfo($resource, CURLINFO_HTTP_CODE);
+// echo $responseCode;
 
 // Create User
 $user = [
-    'name' => 'John Doe',
-    'username' => 'john',
-    'email' => 'john@example.com'
+    'name' => 'pHAM tHANH dANH',
+    'username' => 'THANHDANH',
+    'email' => 'DANH@example.com'
 ];
 
 $ch = curl_init($url);
